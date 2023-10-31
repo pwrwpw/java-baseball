0b7aade59f052ce2535b177dcbe470b06604b807
pwrwpw <dlgustjr9912@gmail.com>
2023-11-01 05:48:43 +0900

    refactor: 코드를 좀더 간결하게 작성하여 가독성을 향상시킴

--- src/main/java/baseball/utils/RandomGenerator.java
+++ src/main/java/baseball/utils/RandomGenerator.java
@@ -6,34 +6,27 @@ import static baseball.model.Constant.RANGE_MIN_NUMBER;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RandomGenerator {
 
 	public List<Integer> generateRandomNumbers() {
-		List<Integer> randomNumbers = new ArrayList<>(NUMBER_LENGTH);
-		for (int i = 0; i < NUMBER_LENGTH; i++) {
-			randomNumbers.add(generateUniqueRandomNumber(randomNumbers));
+		Set<Integer> uniqueNumbers = new HashSet<>();
+
+		while (uniqueNumbers.size() < NUMBER_LENGTH) {
+			int randomNumber = generateUniqueRandomNumber(uniqueNumbers);
+			uniqueNumbers.add(randomNumber);
 		}
-		return randomNumbers;
+
+		return new ArrayList<>(uniqueNumbers);
 	}
-
-
-	private int generateUniqueRandomNumber(List<Integer> randomNumbers) {
-		while (true) {
-			int randomNumber = Randoms.pickNumberInRange(RANGE_MIN_NUMBER, RANGE_MAX_NUMBER);
-			if (!isNumberUsed(randomNumbers, randomNumber)) {
-				return randomNumber;
-			}
-		}
-	}
-
-	private boolean isNumberUsed(List<Integer> randomNumbers, int randomNumber) {
-		for (int number : randomNumbers) {
-			if (number == randomNumber) {
-				return true;
-			}
-		}
-		return false;
+	private int generateUniqueRandomNumber(Set<Integer> usedNumbers) {
+		int randomNumber;
+		do {
+			randomNumber = Randoms.pickNumberInRange(RANGE_MIN_NUMBER, RANGE_MAX_NUMBER);
+		} while (usedNumbers.contains(randomNumber));
+		return randomNumber;
 	}
 }
